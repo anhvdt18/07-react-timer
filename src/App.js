12170247ac7c86157c0061563333e5982706539e
Tuan Anh <anhvdt18@gmail.com>
2024-01-08 21:43:19 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useTimer from "./useTimer";
+import { formatTime } from "./formatTime";
 
 function App() {
+  const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App container">
+        <h1>Coder Timer 1</h1>
+        <div className="timer__wrapper">
+          <div className="timer__display">
+            <p>{formatTime(time)}</p>
+          </div>
+          <div className="button__wrapper">
+            <button className="button" onClick={stopTimer}>
+              Stop
+            </button>
+            <button className="button" ref={active} onClick={startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Reset
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="App container">
+        <h1>Coder Timer 2</h1>
+        <div className="timer__wrapper">
+          <div className="timer__display">
+            <p>{formatTime(time)}</p>
+          </div>
+          <div className="button__wrapper">
+            <button className="button" onClick={stopTimer}>
+              Stop
+            </button>
+            <button className="button" ref={active} onClick={startTimer}>
+              Start
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Reset
+            </button>
+            <button className="button" onClick={resetTimer}>
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/useTimer.js
@@ -0,0 +1,32 @@
+import { useState, useRef } from "react";
+
+const useTimer = (ini = 0) => {
+  const [time, setTime] = useState(0);
+
+  const isStart = useRef(true);
+  const active = useRef();
+  const refInterval = useRef(0);
+
+  const startTimer = () => {
+    isStart.current = true;
+    refInterval.current = setInterval(() => {
+      if (isStart.current) {
+        setTime((time) => time + 1);
+      }
+    }, 1000);
+    active.current.disabled = true;
+  };
+  const stopTimer = () => {
+    clearInterval(refInterval.current);
+    isStart.current = false;
+    active.current.disabled = false;
+  };
+  const resetTimer = () => {
+    setTime(0);
+    clearInterval(refInterval.current);
+    active.current.disabled = false;
+  };
+
+  return { time, startTimer, stopTimer, resetTimer, active };
+};
+export default useTimer;
